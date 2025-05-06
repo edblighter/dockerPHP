d41adcb2706fb1172c7a49447ada2b5c392d8c05
Felipe Prestes <mail.ffpl@gmail.com>
2025-05-06 19:21:08 -0400

    Fixed and improved run.sh

--- run.sh
+++ run.sh
@@ -1,40 +1,51 @@
 #!/bin/sh
 
-if [ "$1" == "clear" ]; then
+#set -x 
+
+clear_env() {
     echo "Clearing environment..."
     sudo rm -rf docker-data/caddy/config docker-data/caddy/data docker-data/mysql/mysqldata docker-data/postgres/data docker-data/postgres/db_root_password.txt docker-data/mysql/db_root_password.txt
+    sudo docker stop app_php app_mysql app_postgres app_redis app_phpmyadmin app_dbadmin app_nginx app_caddy app_mailhog
+    sudo docker rm app_php app_mysql app_postgres app_redis app_phpmyadmin app_dbadmin app_nginx app_caddy app_mailhog
+    echo "##### ATTENTION the following commands will erase all stopped docker containers and associated volumes. Say NO if you don't know what to do. ###"
+    sudo docker system prune --all
+    sudo docker volume prune --all
     echo "Environment cleared."
+}
+
+if [ "$1" = "help" ]; then
+    echo "Usage: $0 [clear | help] [caddy|nginx] [mysql|postgres] [up|down]"
+    echo "ex: $0 nginx mysql up  # this will set up the necessary services to run a nginx|mysql|phpmyadmin|redis|mailhog enviroment"
+    exit 0
+fi
+
+if [ "$1" = "clear" ]; then
+    clear_env    
     exit 0
 else
-    export PWD=${PWD}
     export WEB_SERVER=$1
     export DATABASE=$2
     export MODE=$3
 
-    if [ -z "$WEB_SERVER" ]
-    then
+    if [ -z "$WEB_SERVER" ]; then
         echo 'You must specify the name of the web server to be utilized - caddy or nginx'
         exit 1
-    elif [ "$WEB_SERVER" == 'caddy' || "$WEB_SERVER" == 'nginx' ]
-    then
-        if [ -z "$DATABASE" ]
-        then
+    elif [ "$WEB_SERVER" = "caddy" -o "$WEB_SERVER" = "nginx" ]; then
+        if [ -z "$DATABASE" ]; then
             echo 'You must specify the name of the database server to be utilized - mysql or postgres'
             exit 1
-        elif [ "$DATABASE" == "mysql" || "$DATABASE" == "postgres" ]
-            if [ -z "$MODE"]
-            then
+        elif [ "$DATABASE" = "mysql" -o "$DATABASE" = "postgres" ]; then
+            if [ -z "$MODE" ]; then
                 echo 'You must specify mode for the services - up or down'
                 exit 1
-            elif [ "$MODE" == "up" || "$MODE" == "down" ]
-                if [ "$MODE" == "up"]
-                then
+            elif [ "$MODE" = "up" -o "$MODE" = "down" ]; then
+                if [ "$MODE" = "up" ]; then
                     openssl rand -base64 20 > docker-data/$DATABASE/db_root_password.txt
-                    sudo docker compose --env-file .env.app up -d --build
+                    sudo PWD=${PWD} WEB_SERVER="$WEB_SERVER" DATABASE="$DATABASE" docker compose --env-file .env.app up -d --build
                     exit 0
-                else
-                    sudo docker compose --env-file .env.app down
-                    ./run.sh clear
+                elif [ "$MODE" = "down" ]; then
+                    sudo PWD=${PWD} WEB_SERVER="$WEB_SERVER" DATABASE="$DATABASE" docker compose --env-file .env.app down
+                    clear_env
                     exit 0
                 fi
             fi
